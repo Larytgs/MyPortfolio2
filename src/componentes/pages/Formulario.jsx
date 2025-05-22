7ec23a373e033a47b6239348063715b932a7df71
laryTgs <laryssatortato99@gmail.com>
2025-05-22 15:02:53 -0300

    Update Formulario.jsx

--- src/componentes/pages/Formulario.jsx
+++ src/componentes/pages/Formulario.jsx
@@ -3,6 +3,8 @@ import H2 from "../styles/Subtitulo";
 import Form, { Input } from "../styles/Form";
 
 const Formulario = () => {
+  // Declarar uma nova variavel de dados com state e atribuir objetos
+
   return (
     <Section>
       <H2>Entre em contato</H2>
@@ -21,10 +23,10 @@ const Formulario = () => {
             className="p-2 m-1 border rounded-2xl border-[#6ad0ff] bg-slate-950 placeholder-white focus:outline-none focus:ring-2 focus:ring-blue-500 transition"
             type="text"
             name="msg"
-            rows="6"
+            rows="7"
             placeholder="Sua mensagem"
           />
-          <div className="col-span-1 md:col-span-2 flex justify-start">
+          <div className="col-span-1 md:col-span-2 flex justify-center">
             <button
               className="p-2 w-24 text-center border rounded-2xl border-[#6ad0ff] bg-slate-950 hover:bg-[#6ad0ff73] transition "
               type="submit"
