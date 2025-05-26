d225c1107e1b207aa776591fb34bae5d8f242ad1
laryTgs <laryssatortato99@gmail.com>
2025-05-26 12:11:16 -0300

    up

--- api/app.js
+++ api/app.js
@@ -1,7 +1,10 @@
 // Incluir as bibliotecas
 // Gerencia as requisições, rotas e URLs, entre outra funcionalidades
-
 const express = require("express");
+
+// Importar a biblioteca para permitir conexão externa
+const cors = require("cors");
+
 // Chamar a funcao express
 const app = express();
 
@@ -11,6 +14,23 @@ const mensagens = require("./controllers/mensagens");
 // Criar o middleware para receber os dados no corpo da requisicao
 // Middlewar é uma instrução que vai ser executada antes de acessara rota
 app.use(express.json());
+
+// Middleware: permitir requisições externas com CORS (coloque isso sozinho)
+app.use(cors());
+
+// Criar o middleware para permitir requisição externa
+app.use((req, res, next) => {
+  // Qualquer endereço pode fazer requisição
+  res.header("Access-Control-Allow-Origin", "*");
+  // Tipos de método que a API aceita
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+  // Permitir o envio de dados para API
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  // Executar o cors
+  app.use(cors());
+  // Quando não houver erro deve continuar o processamento
+  next();
+});
 
 // Criar as rotas
 app.use("/message", mensagens); //quando o usuario colocar mensagem, ele quer acessar a rota mensagens
