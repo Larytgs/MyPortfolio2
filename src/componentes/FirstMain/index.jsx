d3a5da00b494e7565835ca01cbc1bdbe08a952a2
laryTgs <laryssatortato99@gmail.com>
2025-06-05 14:57:56 -0300

    Update index.jsx

--- src/componentes/FirstMain/index.jsx
+++ src/componentes/FirstMain/index.jsx
@@ -134,7 +134,7 @@ function Firstmain() {
                 </p>
               </div>
               <h5 className="text-sm uppercase font-bold ml-5">
-                Pós-graduação em Desenvolvimento Front-end
+                Pós em Desenvolvimento Front-end
                 <span className="text-[var(--light-gray)] opacity-80 text-[15px] ml-1">
                   - Faculdade Anhanguera
                 </span>
