47d10ac337ba5a894a6e87a92f2c20e01a0649db
laryTgs <laryssatortato99@gmail.com>
2025-06-06 15:41:54 -0300

    atualizando e adicionando mais projetos

--- src/componentes/pages/Projects.jsx
+++ src/componentes/pages/Projects.jsx
@@ -15,7 +15,7 @@ function Projects() {
                 <img
                   className="rounded-2xl w-[500px]"
                   src="/img/projetos/agencia_de_sites.png"
-                  alt="html5"
+                  alt="agencia_de_sites"
                 />
               </div>
               <a
@@ -24,7 +24,7 @@ function Projects() {
                 target="_blank"
               >
                 <TitleH3 className="H3">
-                  Site para divulgar o meu trabalho.
+                  Site para divulgação. <br /> <i>Deploy</i>
                 </TitleH3>
               </a>
             </div>
@@ -59,7 +59,7 @@ function Projects() {
                 target="_blank"
               >
                 <TitleH3 className="H3">
-                  Site para uma Mecânica Automotiva.
+                  Site para uma Mecânica. <br /> <i>Deploy</i>
                 </TitleH3>
               </a>
             </div>
@@ -94,7 +94,7 @@ function Projects() {
                 target="_blank"
               >
                 <TitleH3 className="H3">
-                  Site de Veterinaria, ainda em andamento.
+                  Site de Veterinária. <br /> <i>Deploy</i>
                 </TitleH3>
               </a>
             </div>
@@ -125,9 +125,9 @@ function Projects() {
                 href="https://site-biscoitos-decorados.vercel.app/"
                 target="_blank"
               >
-                <TitleH32 className="biscoitos">
-                  Site Biscoitos Decorados.
-                </TitleH32>
+                <TitleH3 className="H3">
+                  Site Biscoitos Decorados. <br /> <i>Deploy</i>
+                </TitleH3>
               </a>
             </div>
             <div>
@@ -152,7 +152,7 @@ function Projects() {
                 <img
                   className="rounded-2xl w-[500px]"
                   src="/img/projetos/netflix.png"
-                  alt="siteVeterinaria"
+                  alt="Réplica da Netflix"
                 />
               </div>
               <a
@@ -161,7 +161,7 @@ function Projects() {
                 target="_blank"
               >
                 <TitleH3 className="H3">
-                  Réplica da Netflix, em andamento.
+                  Réplica da Netflix. <br /> <i>Deploy</i>
                 </TitleH3>
               </a>
             </div>
@@ -195,7 +195,9 @@ function Projects() {
                 href="https://larytgs.github.io/devConverter/"
                 target="_blank"
               >
-                <TitleH32 className="biscoitos">Conversão de moedas.</TitleH32>
+                <TitleH3 className="H3">
+                  Conversão de moedas. <br /> <i>Deploy</i>
+                </TitleH3>
               </a>
             </div>
             <div>
@@ -220,7 +222,7 @@ function Projects() {
                 <img
                   className="rounded-2xl w-[500px]"
                   src="/img/projetos/EstiloPinterest.png"
-                  alt="siteVeterinaria"
+                  alt="Projeto Estilo Pinterest"
                 />
               </div>
               <a
@@ -229,7 +231,7 @@ function Projects() {
                 target="_blank"
               >
                 <TitleH3 className="H3">
-                  Projeto Estilo Pinterest, em andamento.
+                  Estilo Pinterest. <br /> <i>Deploy</i>
                 </TitleH3>
               </a>
             </div>
@@ -239,7 +241,41 @@ function Projects() {
                   React
                 </li>
                 <li className="text-[0.8em] bg-[#00397fce] text-white p-1 px-2 rounded-lg">
+                  CSS
+                </li>
+                <li className="text-[0.8em] bg-[#00397fce] text-white p-1 px-2 rounded-lg">
                   Tailwindcss
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          {/* Gerenciador de Lista de Tarefas */}
+          <div className="flex-auto text-center my-3 sm:mx-3">
+            <div className="service-proj relative">
+              <div className="card-proj">
+                <img
+                  className="rounded-2xl w-[500px]"
+                  src="/img/projetos/Lista de tarefas.png"
+                  alt="Lista de Tarefas"
+                />
+              </div>
+              <a className="card-title cursor-pointer" href="#" target="_blank">
+                <TitleH3 className="H3">
+                  Lista de Tarefas. <br /> <i>No Deploy</i>
+                </TitleH3>
+              </a>
+            </div>
+            <div>
+              <ul className="flex mt-2 mx-5 gap-2 text-[#797676]">
+                <li className="text-[0.8em] bg-[#00397fce] text-white p-1 px-2 rounded-lg">
+                  React
+                </li>
+                <li className="text-[0.8em] bg-[#00397fce] text-white p-1 px-2 rounded-lg">
+                  CSS
+                </li>
+                <li className="text-[0.8em] bg-[#00397fce] text-white p-1 px-2 rounded-lg">
+                  JSX
                 </li>
               </ul>
             </div>
