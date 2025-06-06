81d959751015d9f68c82fada10737583fa06e1ea
laryTgs <laryssatortato99@gmail.com>
2025-06-06 10:54:55 -0300

    up

--- src/componentes/Footer.jsx
+++ src/componentes/Footer.jsx
@@ -2,8 +2,9 @@ function Footer() {
   const footerStyle = {
     textAlign: "center",
     color: "white",
-    backgroundColor: "#181717",
+    backgroundColor: "#05060c",
     padding: "14px",
+    position: "relative",
   };
 
   return <h1 style={footerStyle}>&copy;2025 Laryssa Tortato Gomes da Silva</h1>;
